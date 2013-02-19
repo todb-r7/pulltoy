974a0be4a13b77e431289627371aeba4d8c7dfe4
Tod Beardsley <todb@metasploit.com>
2013-02-19 16:52:52 -0600

    Collect change data, output as csv

--- pulltoy.rb
+++ pulltoy.rb
@@ -1,16 +1,20 @@
 #!/usr/bin/env ruby
 
-# A totally ghetto method to sort out PRs.
+# A totally ghetto method to sort out PRs. Works for now, totally
+# dependant on GitHub's HTML which could change at any moment.
+# This is a stopgap until I have something using Github's API proper.
 
 require 'fileutils'
 require 'tempfile'
 require 'open-uri'
 require 'nokogiri'
 require 'time'
+require 'csv'
 
 @prs = {}
 @pr_numbers = []
-@docs = {}
+@pr_pages = {}
+@pr_files = {}
 @temp_prs = Tempfile.new("pull-requests")
 
 def get_prs
@@ -19,7 +23,7 @@ def get_prs
 		url = "https://github.com/rapid7/metasploit-framework/pulls?direction=desc&page=#{i}&sort=created&state=open"
 		cmd = "curl -Lo- '#{url}' >> #{@temp_prs.path}"
 		system(cmd)
-		if i >=1
+		if i >=5
 			puts "Breaking!"
 			break
 		end
@@ -28,11 +32,13 @@ end
 
 def parse(i)
 	url = "https://github.com/rapid7/metasploit-framework/pull/#{i}"
-	@docs[i] = Nokogiri::HTML(open(url))
+	url_files = url << "/files"
+	@pr_pages[i] = Nokogiri::HTML(open(url))
+	@pr_files[i] = Nokogiri::HTML(open(url_files))
 end
 
 def get_author(i)
-	doc = @docs[i]
+	doc = @pr_pages[i]
 	doc.css('//p/a').each do |a|
 		if a.to_s.include? "pull-header-username"
 			return a.attributes["href"].value[1,0xffff]
@@ -41,20 +47,19 @@ def get_author(i)
 end
 
 def get_title(i)
-	doc = @docs[i]
+	doc = @pr_pages[i]
 	doc.css('h2').first.children.to_s
 end
 
-def get_file_count(i)
-
-end
-
-def get_commit_count(i)
-
+def get_line_count(i)
+	doc = @pr_files[i]
+	changes = doc.css('span[@class = "diffstat tooltipped downwards"]').first['title']
+	add_del = changes.scan(/\d+/).map {|i| i.to_i}
+	add_del.inject {|sum, i| sum += i}
 end
 
 def get_date(i)
-	doc = @docs[i]
+	doc = @pr_pages[i]
 	date = Time.parse(doc.css('time').first.attributes['datetime'].value)
 	date.strftime("%Y-%m-%d")
 end
@@ -71,13 +76,17 @@ end
 
 @pr_numbers.sort!
 
+csv_title = %w{Pull, Changes, Author, URL, Title, Date}.to_csv
+puts csv_title
 @pr_numbers.each do |pr|
 	parse(pr)
 	author = get_author(pr)
 	url = "https://github.com/rapid7/metasploit-framework/pull/#{pr}"
 	title = get_title(pr)
 	date = get_date(pr)
-	puts [pr,author,url,title,date].inspect
+	lines = get_line_count(pr)
+	this_pr = [pr,lines,author,url,title,date]
+	puts this_pr.inspect
+	puts this_pr.to_csv
+	$stdout.flush
 end
-
-
